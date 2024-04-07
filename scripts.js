26240ec6064c5e32a6f3b31dba39443d2649b31b
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2024-04-06 22:40:37 -0700

    Got new structure and functions to behave nicely, and implemented dynamic modification of HTML using JS.

--- scripts.js
+++ scripts.js
@@ -31,21 +31,60 @@
 var cardContainer = null;
 const templateCard = null;
 
-const FRESH_PRINCE_URL =
-    "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-    "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-    "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+//#endregion
 
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+///////////////////////////////
+///// CATALOG DATA / FXNS /////
+///////////////////////////////
+//#region CATALOG DATA / FXNS
+
+function catalogInstance(title, url, bp0, bp1, bp2) {
+    return {
+        title,
+        url,
+        bullet_0: bp0,
+        bullet_1: bp1,
+        bullet_2: bp2,
+    };
+}
+
+// Catalog data format:
+//    {
+//        CATEGORY_0: [ CARD00, CARD01, CARD02, ... ]
+//        CATEGORY_1: [ CARD10, CARD11, CARD12, ... ]
+//        CATEGORY_2: [ CARD20, CARD21, CARD22, ... ]
+//    }
+var catalogData = {
+    "Programming Languages": {
+        mask: true,
+        instances: [
+            catalogInstance(
+                "C++",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png",
+                "Perhaps my favorite language",
+                "Strongly typed",
+                "Highly feature-rich"
+            ),
+        ],
+    },
+    Animals: {
+        mask: true,
+        instances: [
+            catalogInstance(
+                "Cats",
+                "https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg"
+            ),
+            catalogInstance(
+                "Dogs",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Labrador_Retriever_portrait.jpg/290px-Labrador_Retriever_portrait.jpg"
+            ),
+        ],
+    },
+};
+
+function applyFilter(category, enable) {
+    catalogData[category].mask = enable;
+}
 
 //#endregion
 
@@ -55,7 +94,6 @@ let titles = [
 //#region FUNCTIONS
 
 function windowLoaded() {
-    console.log("wkhbdfkjghdfkjghdfkjg");
     initGlobals();
     refreshCards();
 }
@@ -76,40 +114,51 @@ function clearCards() {
 }
 
 function showCards() {
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        this.cardContainer.appendChild(createCard(title, imageURL)); // Add new card to the container
+    for (const [_, categoryData] of Object.entries(this.catalogData)) {
+        // This here is the application of the filter
+        //     when rendering.
+        if (!categoryData.mask) continue;
+        showCardsFromCategory(categoryData);
     }
 }
 
-function createCard(newTitle, newImageURL, bp1, bp2, bp3) {
+function showCardsFromCategory(category) {
+    for (const instance of category.instances) {
+        this.cardContainer.appendChild(createCard(instance)); // Add new card to the container
+    }
+}
+
+function createCard(instance) {
     const card = this.templateCard.cloneNode(true);
 
     card.style.display = "block";
 
     const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
+    cardHeader.textContent = instance.title;
 
     const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
+    cardImage.src = instance.url;
+    cardImage.alt = instance.title + " Poster";
+
+    const cardBPs = card.querySelector("ul");
+    cardBPs.innerHTML = "";
+
+    // Tiny helper for adding bullet point
+    //     to unordered list.
+    function addBulletPoint(text) {
+        const bp = document.createElement("li");
+        bp.appendChild(document.createTextNode(text));
+        cardBPs.appendChild(bp);
+    }
+
+    addBulletPoint(instance.bullet_0);
+    addBulletPoint(instance.bullet_1);
+    addBulletPoint(instance.bullet_2);
 
     // You can use console.log to help you debug!
     // View the output by right clicking on your website,
     // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+    console.log("new card:", instance.title, "- html: ", card);
 
     return card;
 }
