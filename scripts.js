2b57634a3b2a8bb65c6f3ced09f862a23efb3dae
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2024-04-06 19:14:14 -0700

    Added prettier formatting, and formatted all files to industry standard.

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@
  * also make changes to the HTML and CSS files, but we want you to prioritize
  * demonstrating your understanding of data structures, and you'll do that
  * with the JavaScript code you write in this file.
- * 
+ *
  * The comments in this file are only to help you learn how the starter code
  * works. The instructions for the project are in the README. That said, here
  * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
+ * - 1 - Change something small in index.html or style.css, then reload your
+ *    browser and make sure you can see that change.
  * - 2 - On your browser, right click anywhere on the page and select
  *    "Inspect" to open the browser developer tools. Then, go to the "console"
  *    tab in the new window that opened up. This console is where you will see
@@ -20,30 +20,31 @@
  * - 3 - Add another string to the titles array a few lines down. Reload your
  *    browser and observe what happens. You should see a fourth "card" appear
  *    with the string you added to the array, but a broken image.
- * 
+ *
  */
 
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+const FRESH_PRINCE_URL =
+    "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
+const CURB_POSTER_URL =
+    "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
+const EAST_LOS_HIGH_POSTER_URL =
+    "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
 // This is an array of strings (TV show titles)
 let titles = [
     "Fresh Prince of Bel Air",
     "Curb Your Enthusiasm",
-    "East Los High"
+    "East Los High",
 ];
-// Your final submission should have much more data than this, and 
+// Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
-
 
 // This function adds cards the page to display the data in the array
 function showCards() {
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
     const templateCard = document.querySelector(".card");
-    
+
     for (let i = 0; i < titles.length; i++) {
         let title = titles[i];
 
@@ -84,11 +85,42 @@ function editCardContent(card, newTitle, newImageURL) {
 document.addEventListener("DOMContentLoaded", showCards);
 
 function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+    console.log("Button Clicked!");
+    alert(
+        "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
+    );
 }
 
 function removeLastCard() {
     titles.pop(); // Remove last item in titles array
     showCards(); // Call showCards again to refresh
 }
+
+const fetchRepoFolders = async (owner, repo, path = "") => {
+    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+    const response = await fetch(apiUrl, {
+        headers: {
+            Accept: "application/vnd.github.v3+json",
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch repository contents");
+    }
+
+    const data = await response.json();
+
+    const folders = data
+        .filter((item) => item.type === "dir")
+        .map((folder) => folder.name);
+    return folders;
+};
+
+// Example usage
+fetchRepoFolders("username", "repo_name")
+    .then((folders) => {
+        console.log("Folders:", folders);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
